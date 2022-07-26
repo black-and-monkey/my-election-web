c1b80e427a990d74ddb67cbc04e3587c75b170a3
jgarbora <juan.garbora@gobio.com>
2022-07-26 14:43:18 -0500

    package json

--- server.js
+++ server.js
@@ -1,34 +1,19 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const { join } = require('path');
-const authConfig = require('./auth_config.json');
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
+const express = require('express');
 const app = express();
 
-const port = process.env.SERVER_PORT || 4200;
+app.use(requireHTTPS);
+app.use(express.static('./dist/angular-heroku'));
 
-app.use(morgan('dev'));
-
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      // reportOnly: true,
-      directives: {
-        'default-src': ["'self'"],
-        'connect-src': ["'self'", 'https://*.auth0.com', authConfig.apiUri],
-        'frame-src': ["'self'", 'https://*.auth0.com'],
-        'base-uri': ["'self'"],
-        'block-all-mixed-content': [],
-        'font-src': ["'self'", 'https:', 'data:'],
-        'frame-ancestors': ["'self'"],
-        'img-src': ["'self'", 'data:', '*.gravatar.com'],
-        'style-src': ["'self'", 'https:', "'unsafe-inline'"],
-      },
-    },
-  })
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/login-demo/'}),
 );
 
-app.use(express.static(join(__dirname, 'dist')));
-
-app.listen(port, () => console.log(`App server listening on port ${port}`));
+app.listen(process.env.PORT || 8080);
